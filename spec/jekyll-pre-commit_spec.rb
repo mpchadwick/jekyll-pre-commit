3f616e725b9b07e9c1f882a5cb694c5b5cbba242
Max Chadwick <mpchadwick@gmail.com>
2017-01-06 08:50:04 -0500

    Add first test

--- spec/jekyll-pre-commit_spec.rb
+++ spec/jekyll-pre-commit_spec.rb
@@ -1,1 +1,19 @@
 require 'spec_helper'
+
+describe(Jekyll::PreCommit::Runner) do
+  let(:config) do
+    Jekyll.configuration({
+        "source" => source_dir
+      })
+  end
+  let(:site) { Jekyll::Site.new(config) }
+  let(:runner) { Jekyll::PreCommit::Runner.new }
+  before(:each) do
+    site.reset
+    site.read
+  end
+  it "returns no messages if no posts are staged" do
+    result = runner.run(site, ["spec/fixtures/favicon.ico"])
+    expect(result).to match_array([])
+  end
+end
