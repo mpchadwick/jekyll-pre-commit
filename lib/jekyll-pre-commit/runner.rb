8876f3044da72b57ff6326b4975499bf75d903fd
Max Chadwick <mpchadwick@gmail.com>
2017-01-06 18:47:27 -0500

    Send messages about OK pre-commit runs
    We can test better this way.

--- lib/jekyll-pre-commit/runner.rb
+++ lib/jekyll-pre-commit/runner.rb
@@ -3,27 +3,36 @@ module Jekyll
     class Runner
 
       def run(site, staged_files)
-        messages = Array.new
+        result = { :ok => true, :messages => [] }
+        if !site.config["pre-commit"] || site.config["pre-commit"].empty?
+          # Bail if there are no pre-commit checks enabled
+          result[:messages].push("No pre-commit checks enabled")
+          return result
+        end
+
         staged_posts = Array.new
         site.posts.docs.each do |p|
           if (staged_files.include? p.path.gsub(Dir.pwd + "/", ""))
             staged_posts.push(p)
           end
         end
+
         if staged_posts.empty?
           # If no posts are being committed. Safe to commit.
-          return messages
+          result[:messages].push("No posts staged")
+          return result
         end
 
         site.config["pre-commit"].each do |c|
           o = Object.const_get("Jekyll::PreCommit::Check::" + c).new
-          result = o.Check(staged_posts, site)
-          if !result[:ok]
-            messages.push(result[:message])
+          r = o.Check(staged_posts, site)
+          if !r[:ok]
+            result[:ok] = false
+            result[:messages].push(r[:message])
           end
         end
 
-        return messages
+        return result
       end
     end
   end
