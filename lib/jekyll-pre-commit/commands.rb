a455d221e7004498a6a6fe45ce27f95be64f4f13
Max Chadwick <mpchadwick@gmail.com>
2017-01-06 21:53:13 -0500

    Add init command

--- lib/jekyll-pre-commit/commands.rb
+++ lib/jekyll-pre-commit/commands.rb
@@ -4,11 +4,30 @@ module Jekyll
       class << self
         def init_with_program(prog)
           prog.command(:precommit) do |c|
-            c.syntax 'hello'
-            c.description 'Says hello'
+            c.syntax 'precommit SUBCOMMAND'
+            c.description 'Runs SUBCOMMAND'
+            c.option "force", "--force", "Overwrites an existing pre-commit if one already exists"
 
             c.action do |args, options|
-              Jekyll.logger.info "Precommit"
+              raise ArgumentError, "You must provide a subcommand" if args.empty?
+              if args.include? "init"
+                dest = "#{Dir.pwd}/.git/hooks/pre-commit"
+                if (File.exist?(dest) && !options["force"])
+                  Jekyll.logger.abort_with "An existing pre-commit file already exists. If you'd like" \
+                    " to overwrite that file (e.g. you're upgrading) pass the --force flag."
+                end
+
+                if (File.exist?(dest))
+                  File.delete(dest)
+                end
+
+                # Is there a better way to resolve this path?
+                src = `bundle show jekyll-pre-commit`
+                src = src.strip + "/lib/jekyll-pre-commit/pre-commit"
+
+                File.symlink src, dest
+                puts "pre-commit hook file created!"
+              end
             end
           end
         end
