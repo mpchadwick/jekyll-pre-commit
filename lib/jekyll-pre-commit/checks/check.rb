e69ce0d762028725525941f59e8cd0b35d69ed9a
Max Chadwick <mpchadwick@gmail.com>
2017-01-11 23:00:51 -0500

    Fix some rubocop issues

--- lib/jekyll-pre-commit/checks/check.rb
+++ lib/jekyll-pre-commit/checks/check.rb
@@ -1,9 +1,11 @@
 module Jekyll
   module PreCommit
     module Checks
+      # The baseline check class
+      # All checks should extend this class
       class Check
         def initialize
-          @result = { :ok => true, :message => "" }
+          @result = { ok: true, message: '' }
         end
       end
     end
