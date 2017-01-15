ab44bcf9e8fbb9718452977fa51032b8152f796e
Max Chadwick <mpchadwick@gmail.com>
2017-01-14 23:00:19 -0500

    Initial progress on spell check

---
+++ lib/jekyll-pre-commit/checks/spell_check.rb
@@ -0,0 +1,18 @@
+module Jekyll
+  module PreCommit
+    module Checks
+      # This is a work-in-progress.
+      class SpellCheck < Check
+        def check(staged, not_staged, site, args)
+          staged.each do |post|
+            rendered = Jekyll::Renderer.new(site, post).run
+            rendered.gsub!('"', '\"')
+            mispells = `echo "#{rendered}" | aspell list -H --add-html-skip=pre --add-html-skip=code`
+            puts mispells
+          end
+          exit 1
+        end
+      end
+    end
+  end
+end
